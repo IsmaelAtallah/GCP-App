99417e6a1cd1d5bf8dd83b45d9ffdc77c78cec4e
IsmaelAtallah <ismaelatallah59@gmail.com>
2022-01-27 18:12:52 +0200

    final edit

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("ismael-lab-d5b80d24b855.json")}"
+  credentials = "${file("ismael-lab-d6dd082c0a4e.json")}"
   project     = "ismael-lab"
   region      = "australia-southeast1"
 }
